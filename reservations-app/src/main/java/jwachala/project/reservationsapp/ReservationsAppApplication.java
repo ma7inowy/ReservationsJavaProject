e9d5f9128a31e8ee5ac471288dc692f646094d0b
ma7inowy <jakubwachala@wp.pl>
2020-07-02 21:22:41 +0200

    Empty springboot project

---
+++ reservations-app/src/main/java/jwachala/project/reservationsapp/ReservationsAppApplication.java
@@ -0,0 +1,13 @@
+package jwachala.project.reservationsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsAppApplication.class, args);
+	}
+
+}
---
+++ reservations-app/src/test/java/jwachala/project/reservationsapp/ReservationsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jwachala.project.reservationsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
